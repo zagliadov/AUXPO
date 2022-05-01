f5cbb8ded483bf141fbefd67e4e184b551fc0441
Daniil Zahliadov <portal.sid.grand2@gmail.com>
2022-05-01 07:36:23 +0300

    first commit

---
+++ js/3-modules.js
@@ -0,0 +1,10 @@
+// Modules
+const {john, peter} = require('./4-names');
+const sayHi = require('./5-utils')
+const data = require('./6-alternative-flavore');
+require('./7-mind-grenade')
+
+
+sayHi('susan');
+sayHi(john);
+sayHi(peter);
---
+++ js/4-names.js
@@ -0,0 +1,9 @@
+const secret = 'SUPER SECRET';
+const john = 'john';
+const peter = 'peter';
+
+
+module.exports = {
+  john, 
+  peter
+}
