f302d71190f42aa39a14100b03cb1ff8fdd4c77b
Daniil Zahliadov <portal.sid.grand2@gmail.com>
2022-05-14 10:30:23 +0300

    input:invalid

--- js/index.js
+++ js/index.js
@@ -1,24 +1,14 @@
-const http = require('http');
-const path = require("path");
+const warningText = document.querySelector("#warningText"),
+  inp = document.querySelector("#inp");
 
-const encoding = "utf8";
-const firstFilePath = path.resolve(__dirname, "content", "first.txt");
-const secondFilePath = path.resolve(__dirname, "content", "second.txt");
+const setErrorText = () => {
+  warningText.innerHTML = "Enter lowercase alphabets only";
+};
 
-const server = http.createServer((req, res) => {
-  if(req.url === '/') {
-    res.end('Welcome to our home page');
-  }
-  if(req.url === '/about') {
-    res.end('Here is your short history');
-  }
-  res.end(`
-    <h1>Oops!</h1>
-    <p>We can't seem to finde the page you are looking for</p>
-    <a href="/">Back home</a>
-  `);
-});
-
-server.listen(5000, () => {
-  console.log('Server run in 5000')
-});
+const delErrorText = () => {
+  setTimeout(() => {
+    warningText.innerHTML = "";
+  }, 2000);
+};
+inp.addEventListener("animationstart", () => setErrorText());
+inp.addEventListener("animationend", () => delErrorText());
